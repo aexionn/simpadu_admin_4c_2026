a687705c43dac819aaecb8fe361dd31027959989
aexionn <corpsesbag@gmail.com>
2026-06-07 23:08:44 +0800

    fix more

--- app/Http/Requests/JurusanStoreRequest.php
+++ app/Http/Requests/JurusanStoreRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class jurusantoreRequest extends FormRequest
+class JurusanStoreRequest extends FormRequest
 {
     public function authorize(): bool { return true; }
 
--- app/Http/Requests/ProdiStoreRequest.php
+++ app/Http/Requests/ProdiStoreRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class proditoreRequest extends FormRequest
+class ProdiStoreRequest extends FormRequest
 {
     public function authorize(): bool { return true; }
 
