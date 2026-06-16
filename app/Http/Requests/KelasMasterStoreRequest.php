620b9b0701674839bcc8a77138c067c1f07ef4c4
aexionn <corpsesbag@gmail.com>
2026-06-16 17:26:48 +0800

    change: rules in kelasmasterstore

--- app/Http/Requests/KelasMasterStoreRequest.php
+++ app/Http/Requests/KelasMasterStoreRequest.php
@@ -14,7 +14,7 @@ class KelasMasterStoreRequest extends FormRequest
         return [
             'NO_ABSEN'          => 'required|integer|min:1',
             'ID_KELAS'          => 'required|integer|exists:kelas,ID_KELAS',
-            'NIM'               => 'required|string|min:10|max:11',
+            'NIM'               => 'required|string|min:9|max:11',
             'ID_TAHUN_AKADEMIK' => 'required|integer|exists:tahun_akademik,ID_TAHUN_AKADEMIK',
             'STATUS_MHS'     => 'sometimes|in:Y,T',
         ];
